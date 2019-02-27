2f8da21eb0d2c96fb858135e2232c8ada9e9d786
Unknown <tvaliaschvili1234@gmail.com>
2019-02-27 11:28:41 +0300

    add db

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require('express'),
       passport = require('passport'),
-      axios = require('axios')
+      axios = require('axios'),
+      body_parser = require('body-parser')
+      db = require('./db')
 
+const app = express()
+app.use(body_parser.json())
+
+db.connect('mongodb://heroku_hjtggkpb:60etht0rf7p90umhl1medqe1jo@ds155045.mlab.com:55045/heroku_hjtggkpb', 'heroku_hjtggkpb', (err) => {
+    if(err){
+        console.log(err)
+    }
+    app.listen(conf.port)
+})
