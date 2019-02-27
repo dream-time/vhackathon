17c64a25e38f2079d85d7acd1c30b0d145e7caaf
Unknown <tvaliaschvili1234@gmail.com>
2019-02-27 14:29:01 +0300

    rt

--- controllers/itemsControllers.js
+++ controllers/itemsControllers.js
@@ -18,7 +18,7 @@ exports.getLkr = (req, res) => {
                 itm = item
             }
         })
-        return itm ? res.send(JSON.parse(itm)) : 'error'
+        return itm ? res.send(JSON.stringify(itm)) : 'error'
     })
 }
 
