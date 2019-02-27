73cc6a1fe047026474f2b22b744f74b838671dd1
Unknown <tvaliaschvili1234@gmail.com>
2019-02-27 12:12:23 +0300

    heroku

--- controllers/user.js
+++ controllers/user.js
@@ -9,34 +9,33 @@ const items = require('../models/user')
 exports.add = (req, res) => {
     let yn = true
     let user = req.body
-    var alls = []
+    var alls;
     items.all((err, docs) => {
         if(err){
             return res.sendStatus(500)
         }
-        alls = docs
-        console.log(docs)
-    })
-    console.log(alls)
-    alls.map((item, index) => {
-        console.log(item.login.toString().trim(),user.login.toString().trim())
-        if(item.login.toString().trim() == user.login.toString().trim()){
-            yn = false
+        docs.map((item, index) => {
+            console.log(item.login.toString().trim(),user.login.toString().trim())
+            if(item.login.toString().trim() == user.login.toString().trim()){
+                yn = false
+            }
+        })
+        if(yn){
+            items.add({
+                login: req.body.login,
+                pass: req.body.pass,
+            }, (err, result) => {
+                if(err) {
+                    console.log(err)
+                    return res.sendStatus(500)
+                }
+                res.sendStatus(200)
+            })
+        }
+        else {
+            res.send('exist')
         }
     })
-    if(yn){
-        items.add({
-            login: req.body.login,
-            pass: req.body.pass,
-        }, (err, result) => {
-            if(err) {
-                console.log(err)
-                return res.sendStatus(500)
-            }
-            res.sendStatus(200)
-        })
-    }
-    else {
-        res.send('exist')
-    }
+    console.log(alls)
+   
 }
